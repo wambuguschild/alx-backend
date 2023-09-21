ca948a312f1a4c5eddc40f87fc79136c495549e4
Jedidah <jedidahwambuguw@gmail.com>
2023-09-21 23:59:44 +0300

    Added 1. Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -14,6 +14,7 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
+
     return (start_index, end_index)
 
 
@@ -37,9 +38,6 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        get page contents
-        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -51,6 +49,6 @@ class Server:
         if start_index >= total_items:
             return []
 
-        end_index = min(end_index, total_items - 1)
+        end_index = min(end_index, total_items)
 
-        return dataset[start_index:end_index + 1]
+        return dataset[start_index:end_index]
