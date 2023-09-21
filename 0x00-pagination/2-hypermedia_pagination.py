5b0d6b4afe84ef9ef39a25ce6170d6b72ac04f9c
Jedidah <jedidahwambuguw@gmail.com>
2023-09-22 00:26:01 +0300

    Added task 2. Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -9,6 +9,11 @@ index_range = __import__('0-simple_helper_function').index_range
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
